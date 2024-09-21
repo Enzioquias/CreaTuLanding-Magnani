3d9cf97aedd6d024d04b4d0dcc10b987f5231e47
Enzo Magnani <magnani_93@hotmail.com>
2024-09-21 19:58:03 -0300

    corregidos algunos estilos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,15 +2,16 @@ import CartWidget from "../CartWidget/CartWidget";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="NavBar">
       <h3>Avicola Frutos del Nido</h3>
-      <div>
+      <div className="NavBarButtons">
         <button>Balanceados</button>
         <button>Animales de Granja</button>
         <button>Insumos</button>
         <br></br>
-        <CartWidget />
+        
       </div>
+      <CartWidget />
     </nav>
   );
 };
